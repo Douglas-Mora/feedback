e0dd1b42ddc2475c6986478681741aeea0054e94
Douglas <doug-mora@outlook.com>
2023-05-04 08:19:42 -0600

    review list

--- reviews/views.py
+++ reviews/views.py
@@ -1,8 +1,10 @@
+from typing import Any, Dict
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 from .forms import ReviewForm
 from django.views.generic.base import TemplateView
+from .models import Review
 
 #from .models import Review
 
@@ -24,27 +26,23 @@ class ReviewView(View):
 
 
 # Create your views here.
-# This next function was replaced by the class ReviewView:
-"""def review(request):
-    if request.method == "POST":
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            #These commented lines correspond to a previous version, using the Review class without the Form class.
-            #user_name = form.cleaned_data['user_name'],
-            #review_text=form.cleaned_data['review_text'],
-            #rating=form.cleaned_data['rating']
-            #)
-            #review.save()
-            form.save()
-            #print(form.cleaned_data)
-            return HttpResponseRedirect("/thank-you")
-    else:
-        form = ReviewForm()
-    return render(request, "reviews/review.html", {
-        "form": form
-    })"""
-
 
 class ThankyouView(TemplateView):
     template_name = "reviews/thank_you.html"
+    
+    def get_context_data(self, **kwargs: Any):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "This works!"
+        return context
+    
+
+class ReviewsListView(TemplateView):
+     template_name = "reviews/review_list.html"
+     
+     def get_context_data(self, **kwargs):
+         context = super().get_context_data(**kwargs)
+         reviews = Review.objects.all()
+         context["reviews"] = reviews
+         return context
+     
     
