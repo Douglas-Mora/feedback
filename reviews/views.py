e20efd6ae7fe1aaf4a2a60a17c0be487042b9fa5
Douglas <doug-mora@outlook.com>
2023-05-04 16:13:55 -0600

    adding listview

--- reviews/views.py
+++ reviews/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.views import View
 from .forms import ReviewForm
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 from .models import Review
 
 #from .models import Review
@@ -36,13 +37,19 @@ class ThankyouView(TemplateView):
         return context
     
 
-class ReviewsListView(TemplateView):
+class ReviewsListView(ListView):
      template_name = "reviews/review_list.html"
-     
-     def get_context_data(self, **kwargs):
-         context = super().get_context_data(**kwargs)
-         reviews = Review.objects.all()
-         context["reviews"] = reviews
-         return context
+     model = Review
+     context_object_name = "reviews"
+
      
     
+class SingleReviewView(TemplateView):
+    template_name = "reviews/single_review.html"
+
+    def get_context_data(self, **kwargs):
+         context = super().get_context_data(**kwargs)
+         review_id = kwargs["id"]
+         selected_review = Review.objects.get(pk=review_id)
+         context["review"] = selected_review
+         return context
